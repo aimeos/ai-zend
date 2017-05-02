908b0699552565ccec988cfd0208b06b1c3c23d3
Aimeos <aimeos@aimeos.org>
2017-05-02 13:37:45 +0200

    Fix for PHP 5.4/5.5 compatibility

--- lib/custom/tests/MW/Logger/ZendTest.php
+++ lib/custom/tests/MW/Logger/ZendTest.php
@@ -79,7 +79,7 @@ class ZendTest extends \PHPUnit\Framework\TestCase
 
 	public function testBadPriority()
 	{
-		$this->expectException('\\Aimeos\\MW\\Logger\\Exception');
+		$this->setExpectedException('\\Aimeos\\MW\\Logger\\Exception');
 		$this->object->log( 'error', -1 );
 	}
 }
