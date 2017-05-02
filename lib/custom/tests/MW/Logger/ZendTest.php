de3c8dbca288cd3967666c566dd9e55f5dda74d4
Aimeos <aimeos@aimeos.org>
2017-05-02 13:13:12 +0200

    Improved compatibility to PHPUnit 6.x

--- lib/custom/tests/MW/Logger/ZendTest.php
+++ lib/custom/tests/MW/Logger/ZendTest.php
@@ -79,7 +79,7 @@ class ZendTest extends \PHPUnit\Framework\TestCase
 
 	public function testBadPriority()
 	{
-		$this->setExpectedException('\\Aimeos\\MW\\Logger\\Exception');
+		$this->expectException('\\Aimeos\\MW\\Logger\\Exception');
 		$this->object->log( 'error', -1 );
 	}
 }
